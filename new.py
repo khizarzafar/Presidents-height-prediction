7fb2addaf1ea5e0078c5fe63c8e72510bdb2fe4a
Your Name <khizarzafar19@gmail.com>
2026-02-21 17:01:55 +0500

    readme changes

--- new.py
+++ new.py
@@ -1,14 +1,18 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+# 1. Load data
 data = pd.read_csv("president_heights.csv")
 data.dropna(subset=['height(cm)'], inplace=True) # Data Integrity check
 print(data.head())
 
+# 2. Extract values into a NumPy array
 height = np.array(data["height(cm)"])
 print(height)
 
-# Descriptive Statistics
+# 3. Descriptive Statistics
 stats = {
     "Mean": np.mean(height),
     "Median": np.median(height),
@@ -22,10 +26,7 @@ for label, value in stats.items():
     print(f"{label:15}: {value:.2f} cm")
 
 
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-
+# 4. Advanced Visualization
 sns.set_theme(style="whitegrid")
 plt.figure(figsize=(10, 6))
 
